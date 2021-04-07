fb14fa40889f8b949b6b40a40c0abc1b9d7f41ea
William Perry <durabilliam@gamil.com>
2021-04-07 21:00:16 -0230

    Pair Project

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,35 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
     }
-}
+  }
+  const vertLetters = transpose(letters);
+  if (vertLetters) {
+    const verticalJoin = vertLetters.map(ls => ls.join(''));
+    for (l of verticalJoin) {
+      if (l.includes(word)) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
 
-module.exports = wordSearch
+const transpose = function(letters) {
+  let dimY = letters.length;
+  let dimX = letters[0].length;
+  let rowArray = [];
+  for (let y = 0; y < dimY; y++) {
+    for (let x = 0; x < dimX; x++) {
+      if (y === 0) {
+        rowArray.push([]);
+      }
+      rowArray[x].push(letters[y][x]);
+    }
+  }
+  return rowArray;
+};
+
+module.exports = wordSearch;
